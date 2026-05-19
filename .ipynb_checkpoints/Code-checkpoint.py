00ca0334e33d148b5988167896bb24e951609d52
Arthur Marchier <arthur.marchier@gmail.com>
2026-05-20 00:11:51 +0200

    c'est good jusqu'à 2.1 inclus

--- .ipynb_checkpoints/Code-checkpoint.py
+++ .ipynb_checkpoints/Code-checkpoint.py
@@ -1,4 +1,6 @@
 import CoolProp.CoolProp as CP
+import numpy as np
+import matplotlib.pyplot as plt
 
 
 # Q1.1
@@ -23,60 +25,341 @@ def iteration_T_cond(ecart_min,n_iterations):
 print(iteration_T_cond(1e-3,1000))    
 
 
-#Q1.2
-#Calcul de P_5s : 
-print(CP.PropsSI('P', 'T', -8+273.15, 'Q', 1, 'CO2'))
-
-#Calcul de T_2f :
-print(294.82761632902583 + (CP.PropsSI('C', 'T', 294.82761632902583, 'P', 101300, 'Air')/CP.PropsSI('CP0MASS', 'T', 294.82761632902583, 'Q', 1, 'CO2')) * (20+273.15 - (294.82761632902583-5)))
-
-#Calcul de q_c
-print(-CP.PropsSI('C', 'T', 294.82761632902583, 'P', 101300, 'Air')*5)
-
-
 # +
-def recherche_P1(ecart_min,n_iterations): #servira pour le COP
-    
+#Q1.2
 def COP():
     Tcond,P3=iteration_T_cond(1e-3,1000)
-    h3sv=CP.PropsSI('H', 'T', Tcond, 'Q', 1, 'CO2')
-    h3sl=CP.PropsSI('H', 'T', Tcond, 'Q', 0, 'CO2')
-    h4=CP.PropsSI('H', 'T', Tcond-2, 'Q', 0, 'CO2')
-    s4=CP.PropsSI('S', 'T', Tcond-2, 'Q', 0, 'CO2')
-    #h5=h4 #détente isenthalpique
-    #h5sv=CP.PropsSI('H', 'T', -8+273.15 , 'Q', 1, 'CO2')
-    cp_airA2 = CP.PropsSI('C', 'T', 20+273.15, 'P', 101300, 'Air')
-    s2 = CP.PropsSI('S', 'T', 20 + 273.15, 'P', 101300, 'Air') #s2=sA2
-    P2 = P3 #Pas de pertes de charges
-    T2 = CP.PropsSI('T', 'P', P2, 'S', s2, 'CO2')
-    T1 = -3 + 273.15
-    P1 = P2 * (T1/T2) #Loi des gaz parfaits
-    rho = 0.5*(CP.PropsSI('D', 'P', P1, 'T', T1, 'CO2') + CP.PropsSI('D', 'P', P2, 'T', T2, 'CO2')) #On calcule la masse volumique du CO2
-    w_is = (1/rho)*(P2-P1) #car dh = Tds - vdP et on est dans le cas isentropique
-    w = w_is/(1-0.121*(P2/P1))
-    return -1 + cp_airA2*5/w
+    P5sv=CP.PropsSI('P','T',-8+273.15,'Q',1,'CO2')
+    P1=P5sv #On néglige la perte de charge dans les échangeurs
+    h1=CP.PropsSI('H','P',P1,'T',-3+273.15,'CO2')
+    h4=CP.PropsSI('H','T',Tcond-2,'Q',0,'CO2')
+    h5=h4 #détente isenthalpique
+    qf=h1-h5
+
+    s1=CP.PropsSI('S','P',P1,'T',-3+273.15,'CO2')
+    P2=P3 #on néglige la perte de charges dans l'échangeur
+    s2is=s1
+    h2is=CP.PropsSI('H','S',s2is,'P',P2,'CO2')
+    wis=h2is-h1
+    w=wis/(1-0.121*(P2/P1))
+    return qf/w
 
 print(COP())
 
 
 # +
-#Q2.1
-def calcul_COP(P3):
-    cp_air = CP.PropsSI('C', 'T', 30+273.15, 'P', 101300, 'Air')
-    h3 = CP.PropsSI('H', 'P', P3, 'T', 35+273.15, 'CO2')
-    h4 = h3 #détente isenthalpique
-    h2 = h3 + cp_air * 5
-    s2 = CP.PropsSI('S', 'P', 101300, 'T', 35+273.15, 'Air')
-    P2 = CP.PropsSI('P', 'S', s2, 'H', h2, 'CO2')
-    T2 = CP.PropsSI('T', 'S', s2, 'H', h2, 'CO2')
-    T1 = -8 + 5 + 273.15
-    P1 = P2 * (T1/T2) #On applique la loi des gaz parfaits
-    rho = 0.5*(CP.PropsSI('D', 'P', P1, 'T', T1, 'CO2') + CP.PropsSI('D', 'P', P2, 'T', T2, 'CO2')) #On calcule la masse volumique du CO2
-    w_is = (1/rho)*(P2-P1) #on utilise pour cela que dh = Tds - vdP mais dans le cas isentropique
-    w = w_is/(1-0.121*(P2/P1))
-    return -1 + cp_air * 5/w
+def trace1():
+    Tcond,P3=iteration_T_cond(1e-3,1000)
+    T3sv=Tcond
+    T3sl=Tcond
+    T4=Tcond-2
+    T5=-8+273.15
+    T5sv=T5
+    T1=T5+5
 
-print(calcul_COP(120e5))
+    P5sv=CP.PropsSI('P','T',-8+273.15,'Q',1,'CO2')
+    P1=P5sv #On néglige la perte de charge dans les échangeurs
+    s1=CP.PropsSI('S','P',P1,'T',T1,'CO2')
+    s3sv=CP.PropsSI('S','Q',1,'T',T3sv,'CO2')
+    s3sl=CP.PropsSI('S','Q',0,'T',T3sl,'CO2')
+    s4=CP.PropsSI('S','Q',0,'T',T4,'CO2')
+    s5sv=CP.PropsSI('S','Q',1,'T',T5,'CO2')
+    P5=P5sv #Pas de pertes de charge
+    h4=CP.PropsSI('H','T',Tcond-2,'Q',0,'CO2')
+    h5=h4 #détente isenthalpique
+    s5=CP.PropsSI('S','H',h5,'P',P5,'CO2')
+
+    #On traite le cas du point 2
+    P2=P3 #on néglige la perte de charges dans l'échangeur
+    s2is=s1
+    h2is=CP.PropsSI('H','S',s2is,'P',P2,'CO2')
+    h1=CP.PropsSI('H','P',P1,'T',T1,'CO2')
+    wis=h2is-h1
+    w=wis/(1-0.121*(P2/P1))
+    h2=h1+w
+    s2=CP.PropsSI('S','H',h2,'P',P2,'CO2')
+    T2=CP.PropsSI('T','H',h2,'P',P2,'CO2')
+
+    s=[s1,s2,s3sv,s3sl,s4,s5,s5sv,s1]
+    T=[T1,T2,T3sv,T3sl,T4,T5,T5sv,T1]
+
+    #Pour l'air
+    TA1,TA2=15+273.15,20+273.15
+    p_atm=101300
+    sA1=CP.PropsSI('S','T',TA1,'P',p_atm,'Air')
+    sA2=CP.PropsSI('S','T',TA2,'P',p_atm,'Air')
+    Tair=[TA1,TA2]
+    sair=[sA1,sA2]
+
+    plt.plot(s, T, marker='o', color='green', linewidth=2, markersize=8, label='CO₂')
+    plt.plot(sair, Tair, marker='o', color='red', linewidth=2, markersize=8, label='Air')
+
+    for i, (s_val, T_val) in enumerate(zip(s, T)):
+        plt.scatter(s_val, T_val, color='green')
+        if i in [0,1]:
+            plt.text(s_val, T_val, f' {i+1}', fontsize=12, ha='right', va='bottom', color='green')
+        elif i==2:
+            plt.text(s_val, T_val, f'3sv', fontsize=12, ha='right', va='bottom', color='green')
+        elif i==3:
+            plt.text(s_val, T_val, f'3sl', fontsize=12, ha='right', va='bottom', color='green')
+        elif i in [4,5]:
+            plt.text(s_val, T_val, f' {i}', fontsize=12, ha='right', va='bottom', color='green')
+        elif i==6:
+            plt.text(s_val, T_val, f'5sv', fontsize=12, ha='right', va='bottom', color='green')
+            
+    plt.scatter(sA1, TA1, color='red')
+    plt.text(sA1, TA1, f'A1', fontsize=12, ha='right', va='bottom', color='red')
+    plt.scatter(sA2, TA2, color='red')
+    plt.text(sA2, TA2, f'A2', fontsize=12, ha='right', va='bottom', color='red')
+    
+    plt.xlabel('s (J/kg·K)', fontsize=12)
+    plt.ylabel('T (K)', fontsize=12)
+    plt.title('Diagramme T-s du cycle simple sous-critique', fontsize=14)
+    plt.legend(fontsize=10)  # Affiche la légende "Cycle CO₂"
+    plt.grid(True, alpha=0.3)  # Grille légère
+    
+trace1()
+
+
+# +
+def trace1log():
+    Tcond,P3=iteration_T_cond(1e-3,1000)
+    T3sv=Tcond
+    T3sl=Tcond
+    T4=Tcond-2
+    T5=-8+273.15
+    T5sv=T5
+    T1=T5+5
+
+    P5sv=CP.PropsSI('P','T',-8+273.15,'Q',1,'CO2')
+    P1=P5sv #On néglige la perte de charge dans les échangeurs
+    s1=CP.PropsSI('S','P',P1,'T',T1,'CO2')
+    s3sv=CP.PropsSI('S','Q',1,'T',T3sv,'CO2')
+    s3sl=CP.PropsSI('S','Q',0,'T',T3sl,'CO2')
+    s4=CP.PropsSI('S','Q',0,'T',T4,'CO2')
+    s5sv=CP.PropsSI('S','Q',1,'T',T5,'CO2')
+    P5=P5sv #Pas de pertes de charge
+    h4=CP.PropsSI('H','T',Tcond-2,'Q',0,'CO2')
+    h5=h4 #détente isenthalpique
+    s5=CP.PropsSI('S','H',h5,'P',P5,'CO2')
+
+    #On traite le cas du point 2
+    P2=P3 #on néglige la perte de charges dans l'échangeur
+    s2is=s1
+    h2is=CP.PropsSI('H','S',s2is,'P',P2,'CO2')
+    h1=CP.PropsSI('H','P',P1,'T',T1,'CO2')
+    wis=h2is-h1
+    w=wis/(1-0.121*(P2/P1))
+    h2=h1+w
+    s2=CP.PropsSI('S','H',h2,'P',P2,'CO2')
+    T2=CP.PropsSI('T','H',h2,'P',P2,'CO2')
+
+    s=[s1,s2,s3sv,s3sl,s4,s5,s5sv,s1]
+    T=[T1,T2,T3sv,T3sl,T4,T5,T5sv,T1]
+
+    #Pour l'air
+    TA1,TA2=15+273.15,20+273.15
+    p_atm=101300
+    sA1=CP.PropsSI('S','T',TA1,'P',p_atm,'Air')
+    sA2=CP.PropsSI('S','T',TA2,'P',p_atm,'Air')
+    Tair=[TA1,TA2]
+    sair=[sA1,sA2]
+
+    #Courbe de saturation
+    Tsat=np.linspace(0,31+273.15,1000) #La température critique du CO2 est de 31°C
+    sliq=CP.PropsSI('S','Q',0,'T',Tsat,'CO2')
+    sgaz=CP.PropsSI('S','Q',1,'T',Tsat,'CO2')
+
+    plt.xlim(1e3,4e3)
+    plt.ylim(250,350)
+    plt.xscale('log')
+    plt.plot(s, T, marker='o', color='green', linewidth=2, markersize=8, label='CO₂')
+    plt.plot(sair, Tair, marker='o', color='red', linewidth=2, markersize=8, label='Air')
+    plt.plot(sliq, Tsat, color='black', linewidth=0.5)
+    plt.plot(sgaz, Tsat, color='black', linewidth=0.5)
+
+    for i, (s_val, T_val) in enumerate(zip(s, T)):
+        plt.scatter(s_val, T_val, color='green')
+        if i in [0,1]:
+            plt.text(s_val, T_val, f' {i+1}', fontsize=12, ha='right', va='bottom', color='green')
+        elif i==2:
+            plt.text(s_val, T_val, f'3sv', fontsize=12, ha='right', va='bottom', color='green')
+        elif i==3:
+            plt.text(s_val, T_val, f'3sl', fontsize=12, ha='right', va='bottom', color='green')
+        elif i in [4,5]:
+            plt.text(s_val, T_val, f' {i}', fontsize=12, ha='right', va='bottom', color='green')
+        elif i==6:
+            plt.text(s_val, T_val, f'5sv', fontsize=12, ha='right', va='bottom', color='green')
+            
+    plt.scatter(sA1, TA1, color='red')
+    plt.text(sA1, TA1, f'A1', fontsize=12, ha='right', va='bottom', color='red')
+    plt.scatter(sA2, TA2, color='red')
+    plt.text(sA2, TA2, f'A2', fontsize=12, ha='right', va='bottom', color='red')
+    
+    plt.xlabel('s (J/kg·K)', fontsize=12)
+    plt.ylabel('T (K)', fontsize=12)
+    plt.title('Diagramme T-s du cycle simple sous-critique avec abscisses en échelle log', fontsize=14)
+    plt.legend(fontsize=10)  # Affiche la légende "Cycle CO₂"
+    plt.grid(True, alpha=0.3)  # Grille légère
+    
+trace1log()
+
+
+# +
+#Q2.1
+def COP2(P3):
+    T3=35+273.15
+    h1=CP.PropsSI('H','T',-8+273.15,'Q',1,'CO2')
+    h3=CP.PropsSI('H','T',T3,'P',P3,'CO2')
+    h5=h3 #détente isenthalpique
+    qf=h1-h5
+
+    P2=P3 #Pas de perte de charges dans le refroidisseur
+    P1=CP.PropsSI('P','T',-8+273.15,'Q',1,'CO2')
+    s1=CP.PropsSI('S','T',-8+273.15,'Q',1,'CO2')
+    s2is=s1
+    h2is=CP.PropsSI('H','P',P2,'S',s2is,'CO2')
+    wis=h2is-h1
+    w=wis/(1-0.121*(P2/P1))
+    return qf/w
+    
+print(COP2(120e5))
+
+
+# +
+def maximiseur_COP():
+    dP = 10e5
+    P_opt = 120e5
+    COP_opt = COP2(P_opt)
+
+    while dP > 1e2:
+        P_test = P_opt + dP
+        COP_test = COP2(P_test)
+        
+        if COP_test > COP_opt:
+            P_opt = P_test
+            COP_opt = COP_test
+            
+        else:
+            P_test = P_opt - dP
+            COP_test = COP2(P_test)
+            
+            if COP_test > COP_opt:
+                P_opt = P_test
+                COP_opt = COP_test
+                
+            else: #Cas où aucun des deux côtés n'améliore le COP
+                dP /= 2
+
+    return P_opt
+
+maximiseur_COP()
+
+
+# +
+def trace2():
+    P3=maximiseur_COP()
+    T3=35+273.15
+    T1=-8+273.15
+    T5=T1
+    h3=CP.PropsSI('H','T',T3,'P',P3,'CO2')
+    h4=h3 #détente isenthalpique
+    #T4=CP.PropsSI('T','H',h4,'Q',0,'CO2') --> nécessaire de connaître P4 pour calculer
+
+    s1=CP.PropsSI('S','T',T1,'Q',1,'CO2')
+    P1=CP.PropsSI('P','T',T1,'Q',1,'CO2')
+    s3=CP.PropsSI('S','T',T3,'P',P3,'CO2')
+    #s4=CP.PropsSI('S','H',h4,'Q',0,'CO2')
+    P5=P1 #Pas de perte de charges dans l'échangeur
+    h5=h3 #détente isenthalpique
+    s5=CP.PropsSI('S','P',P5,'H',h5,'CO2')
+
+    #Cas du point 2
+    P2=P3 #Pas de perte de charges dans le refroidisseur
+    s2is=s1
+    h2is=CP.PropsSI('H','P',P2,'S',s2is,'CO2')
+    h1=CP.PropsSI('H','P',P1,'S',s1,'CO2')
+    wis=h2is-h1
+    w=wis/(1-0.121*(P2/P1))
+    h2=h1+w
+    s2=CP.PropsSI('S','P',P2,'H',h2,'CO2')
+    T2=CP.PropsSI('T','P',P2,'H',h2,'CO2')
+
+    s=[s1,s2,s3,s5,s1]
+    T=[T1,T2,T3,T5,T1]
+    
+    plt.plot(s, T, marker='o', color='green', linewidth=2, markersize=8, label='CO₂')
+    for i, (s_val, T_val) in enumerate(zip(s, T)):
+        if i==4:
+            break
+        plt.scatter(s_val, T_val, color='green')
+        plt.text(s_val, T_val, f' {i+1}', fontsize=12, ha='right', va='bottom', color='green')
+
+    plt.xlabel('s (J/kg·K)', fontsize=12)
+    plt.ylabel('T (K)', fontsize=12)
+    plt.title('Diagramme T-s du cycle transcritique simple', fontsize=14)
+    plt.legend(fontsize=10)  # Affiche la légende "Cycle CO₂"
+    plt.grid(True, alpha=0.3)  # Grille légère
+        
+trace2()
+
+
+# +
+def trace2():
+    P3=maximiseur_COP()
+    T3=35+273.15
+    T1=-8+273.15
+    T5=T1
+    h3=CP.PropsSI('H','T',T3,'P',P3,'CO2')
+    h4=h3 #détente isenthalpique
+    #T4=CP.PropsSI('T','H',h4,'Q',0,'CO2') --> nécessaire de connaître P4 pour calculer
+
+    s1=CP.PropsSI('S','T',T1,'Q',1,'CO2')
+    P1=CP.PropsSI('P','T',T1,'Q',1,'CO2')
+    s3=CP.PropsSI('S','T',T3,'P',P3,'CO2')
+    #s4=CP.PropsSI('S','H',h4,'Q',0,'CO2')
+    P5=P1 #Pas de perte de charges dans l'échangeur
+    h5=h3 #détente isenthalpique
+    s5=CP.PropsSI('S','P',P5,'H',h5,'CO2')
+
+    #Cas du point 2
+    P2=P3 #Pas de perte de charges dans le refroidisseur
+    s2is=s1
+    h2is=CP.PropsSI('H','P',P2,'S',s2is,'CO2')
+    h1=CP.PropsSI('H','P',P1,'S',s1,'CO2')
+    wis=h2is-h1
+    w=wis/(1-0.121*(P2/P1))
+    h2=h1+w
+    s2=CP.PropsSI('S','P',P2,'H',h2,'CO2')
+    T2=CP.PropsSI('T','P',P2,'H',h2,'CO2')
+
+    s=[s1,s2,s3,s5,s1]
+    T=[T1,T2,T3,T5,T1]
+
+    #Courbe de saturation
+    Tsat=np.linspace(0,31+273.15,1000) #La température critique du CO2 est de 31°C
+    sliq=CP.PropsSI('S','Q',0,'T',Tsat,'CO2')
+    sgaz=CP.PropsSI('S','Q',1,'T',Tsat,'CO2')
+
+    plt.xlim(1e3,2.1e3)
+    plt.ylim(250,400)
+    plt.xscale('log')
+    plt.plot(s, T, marker='o', color='green', linewidth=2, markersize=8, label='CO₂')
+    plt.plot(sliq, Tsat, color='black', linewidth=0.5)
+    plt.plot(sgaz, Tsat, color='black', linewidth=0.5)
+    for i, (s_val, T_val) in enumerate(zip(s, T)):
+        if i==4:
+            break
+        plt.scatter(s_val, T_val, color='green')
+        plt.text(s_val, T_val, f' {i+1}', fontsize=12, ha='right', va='bottom', color='green')
+
+    plt.xlabel('s (J/kg·K)', fontsize=12)
+    plt.ylabel('T (K)', fontsize=12)
+    plt.title('Diagramme T-s du cycle transcritique simple avec abscisses en échelle log', fontsize=14)
+    plt.legend(fontsize=10)  # Affiche la légende "Cycle CO₂"
+    plt.grid(True, alpha=0.3)  # Grille légère
+        
+trace2()
 # -
 
 
